220d6a39da82c3f787885eba9cff911122ad4471
lumen-sinthetic <vadimsi2105@gmail.com>
2025-11-06 16:28:04 +0500

    chore: scripts and configs adjusted

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
-	dts: true,
-	sourcemap: true,
-	clean: true,
-	minify: true,
-	outDir: "dist",
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  outDir: "dist",
 });
